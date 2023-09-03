f585e81bbc8cf8e0a14ea0087cb71b7bb8ff5e2e
DonGutierrez <daniex2000@hotmail.com>
2023-09-02 21:57:05 -0300

    agregamoscarrito

--- js/main.js
+++ js/main.js
@@ -1,70 +1,128 @@
-// declaraciones del tablero 
-let turno = 0;
-const tablero = [];
-let jugadores = [
-  { nombre: 'Jugador Rojo', victorias: 0 },
-  { nombre: 'Jugador Azul', victorias: 0 }
-];
-//funcion arrow
-const bPulsado = (e, pos) => {
-  turno++;
-  const b = e.target;
-  const color = turno % 2 ? 'red' : 'blue';
-  b.style.backgroundColor = color;
-  tablero[pos] = color;
-  if (haGanado()) {
-    const jugadorGanador = color === 'red' ? jugadores[0] : jugadores[1];
 
-    jugadorGanador.victorias++;
-    mostrarHistorial();
-    reiniciar();
-  } else if (turno === 9) {
-   
-    reiniciar();
-  }
-};
 
-const haGanado = () => {
-  // Comprobación de líneas horizontales
-  for (let i = 0; i <= 6; i += 3) {
-    if (tablero[i] && tablero[i] === tablero[i + 1] && tablero[i] === tablero[i + 2]) {
-      return true;
+let productos = [];
+
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML ="";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
     }
-  }
 
-  // Comprobación de líneas verticales
-  for (let i = 0; i <= 2; i++) {
-    if (tablero[i] && tablero[i] === tablero[i + 3] && tablero[i] === tablero[i + 6]) {
-      return true;
-    }
-  }
+    actualizarNumerito();
 
-  // Comprobación de diagonales
-  if ((tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]) ||
-      (tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6])) {
-    return true;
-  }
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
 
-  return false;
-};
-//bucle
-const mostrarHistorial = () => {
-  const historialContainer = document.getElementById('historial-container');
-  historialContainer.innerHTML = '';
-  jugadores.forEach(jugador => {
-    const div = document.createElement('div');
-    div.textContent = `${jugador.nombre}: ${jugador.victorias} victorias`;
-    historialContainer.appendChild(div);
-  });
-};
-
-const reiniciar = () => {
-  turno = 0;
-  tablero.length = 0;
-  document.querySelectorAll('button').forEach(b => b.style.backgroundColor = '');
-};
-
-document.querySelectorAll('button').forEach((obj, i) => obj.addEventListener('click', (e) => bPulsado(e, i)));
-
-mostrarHistorial();
-//fin de momento
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
